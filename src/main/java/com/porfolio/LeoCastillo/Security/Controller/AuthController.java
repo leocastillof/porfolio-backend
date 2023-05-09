20a0d10045e1e482a7710bc58f50cc236d28821f
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-09 08:36:47 -0300

    Login and Register class

---
+++ src/main/java/com/porfolio/LeoCastillo/Security/Controller/AuthController.java
@@ -0,0 +1,84 @@
+package com.porfolio.LeoCastillo.Security.Controller;
+
+import com.porfolio.LeoCastillo.Enums.RolName;
+import com.porfolio.LeoCastillo.Security.Entity.Rol;
+import com.porfolio.LeoCastillo.Security.Entity.User;
+import com.porfolio.LeoCastillo.Security.Service.RolServices;
+import com.porfolio.LeoCastillo.Security.Service.UserServices;
+import com.porfolio.LeoCastillo.Segurity.jwt.JwtProvider;
+import jakarta.validation.Valid;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserServices userService;
+    @Autowired
+    RolServices rolService;
+    @Autowired
+    JwtProvider jwtProvider;
+    
+    @PostMapping("/new")
+    public ResponseEntity<?> newUser(@Valid @RequestBody NewUser newUser, BindingResult bindingResult)
+    {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Messager("Las credenciales no son válidas"), HttpStatus.BAD_REQUEST);
+        if(userService.existsByUsername(username.getUsername()))
+            return new ResponseEntity(new Messager("El nombre de usuario ya está en uso"), HttpStatus.BAD_REQUEST);
+        if(userService.existsByEmail(username.getEmail()))
+            return new ResponseEntity(new Messager("El correo electrónico ya está en uso"), HttpStatus.BAD_REQUEST);      
+        User user = new User(newUser.getName(), newUser.getUsername(),
+        newUser.getEmail(), passwordEncoder.encode(newUser.getPassword()));
+        
+        Set<Rol> roles = new HashSet<>();
+        roles.add(rolService.getByName(RolName.ROL_USER).get());
+        
+        if(newUser.getRoles().contains("admin"))
+            roles.add(rolService.getByRolName(RolName.ROLE_ADMIN).get());
+        user.setRoles(roles);
+        userService.save(user);
+        
+        return new ResponseEntity(new Messager("Usuario guardado"), HttpStatus.CREATED);
+    }
+    
+    @PostMapping("/login")
+    public ResponseEntity<JwtDTC> login(@Valid @RequestBody LoginUser loginUser, BindingResult bindingResult)
+    {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Messager("Campos no válidos"), HttpStatus.BAD_REQUEST);
+        
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+        loginUser.getUsername(), loginUser.getPassword()));
+        
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        
+        String jwt = jwtProvider.generateToken(authentication);
+        
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        
+        JwtDto jwtDto = new JwtDto(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+        
+        return new ResponseEntity(jwtDto, HttpStatus.Ok);
+    }
+}
---
+++ src/main/java/com/porfolio/LeoCastillo/Security/Controller/Messager.java
@@ -0,0 +1,27 @@
+package com.porfolio.LeoCastillo.Security.Controller;
+
+public class Messager {
+    private String messager;
+    
+    // Constructor
+    public Messager()
+    {
+        
+    }
+    
+    public Messager(String messager)
+    {
+        this.messager = messager;
+    }
+    
+    // Getter & Setter
+    public String getMessager()
+    {
+        return messager;
+    }
+    
+    public void setMessager(String messager)
+    {
+        this.messager = messager;
+    }
+}
