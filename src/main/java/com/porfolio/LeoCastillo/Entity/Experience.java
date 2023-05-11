59f7fde3dc51276a210282b8a532405e36cf1324
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-11 03:14:57 -0300

    Update Experience.java

--- src/main/java/com/porfolio/LeoCastillo/Entity/Experience.java
+++ src/main/java/com/porfolio/LeoCastillo/Entity/Experience.java
@@ -1,13 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.porfolio.LeoCastillo.Entity;
 
-/**
- *
- * @author leoca
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Experience {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nameE;
+    private String descriptionE;
+    
+    // Constructors
+    public Experience()
+    { 
+    }
+    
+    public Experience(String nameE, String descriptionE)
+    {
+        this.nameE = nameE;
+        this.descriptionE = descriptionE;
+    }
+    
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameE() {
+        return nameE;
+    }
+
+    public void setNameE(String nameE) {
+        this.nameE = nameE;
+    }
+
+    public String getDescriptionE() {
+        return descriptionE;
+    }
+
+    public void setDescriptionE(String descriptionE) {
+        this.descriptionE = descriptionE;
+    }
     
 }
