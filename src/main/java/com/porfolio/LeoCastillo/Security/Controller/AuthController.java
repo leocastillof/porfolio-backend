24206f6dc55b0a58c717d100c870bdf308c255f3
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-12 07:53:39 -0300

    Error

--- src/main/java/com/porfolio/LeoCastillo/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/LeoCastillo/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://porfolio-frontend-leo.web.app")
+@CrossOrigin(origins = "https://porfolio-frontend-leo.web.app/")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
