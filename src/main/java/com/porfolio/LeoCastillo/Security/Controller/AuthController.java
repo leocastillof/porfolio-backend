d16fbc052179b3abe738a625cd0bb96b674e5507
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-12 07:23:52 -0300

    Update

--- src/main/java/com/porfolio/LeoCastillo/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/LeoCastillo/Security/Controller/AuthController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://porfolio-frontend-leo.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://porfolio-frontend-leo.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
