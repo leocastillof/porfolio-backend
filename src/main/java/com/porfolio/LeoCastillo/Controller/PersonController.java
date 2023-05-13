bfede8b858d74a1ecefa42d5ed05393ca8c05ac6
Leo Castillo <88405316+leocastillof@users.noreply.github.com>
2023-05-13 00:38:05 -0300

    Update

--- src/main/java/com/porfolio/LeoCastillo/Controller/PersonController.java
+++ src/main/java/com/porfolio/LeoCastillo/Controller/PersonController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://porfolio-frontend-leo.web.app/")
+@CrossOrigin(origins = {"https://porfolio-frontend-leo.web.app","http://localhost:4200"})
 public class PersonController {
     @Autowired IPersonService ipersonService;
     
